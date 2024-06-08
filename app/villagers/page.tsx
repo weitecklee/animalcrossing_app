c7face368ed650defcaf83bf08c5f75fa5af6c07
arkteck <arkteck@gmail.com>
2024-06-08 09:24:37 -0400

    add loading to villagers

--- app/villagers/page.tsx
+++ app/villagers/page.tsx
@@ -6,25 +6,22 @@ import { Grid } from "@mui/material";
 import Legend from "./legend";
 import { useContext } from "react";
 import { DataContext } from "@/lib/dataContext";
+import Loading from "../loading";
 
 export default function Villagers() {
 
   const { historyMap } = useContext(DataContext);
 
-  if (!historyMap) {
-    return;
-  }
-
   return <>
     <Legend />
     <Grid container spacing={2} py={2} justifyContent='center'>
-      {Array.from(historyMap.values()).map((history) => (
+      {!!historyMap ? Array.from(historyMap.values()).map((history) => (
         <VillagerCard
           key={history.name}
           history={history}
           villagerData={nookipediaData.get(history.name)!}
         />
-      ))}
+      )) : <Grid item xs><Loading /></Grid>}
     </Grid>
     <Legend />
   </>
