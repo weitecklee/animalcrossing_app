bd8a5921babdab6e15ca8073f3710799b28e96ad
arkteck <arkteck@gmail.com>
2024-06-08 14:05:52 -0400

    trying another global-error

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,54 +1,16 @@
 'use client';
 
-import { Grid, Typography } from '@mui/material';
-import ErrorPNG from '../public/avatar19.png';
-import Image from 'next/image';
-import { ThemeProvider } from '@mui/material/styles';
-import { ScreenProvider } from "@/lib/screenContext";
-import { Container, CssBaseline } from "@mui/material";
-import TopBar from "@/lib/topBar";
-import { theme } from "@/lib/theme";
-
-export default function Error({error}: {error: Error & {digest?: string}}) {
-
-  console.log(error);
-
-  return <html lang="en">
-    <body>
-      <ThemeProvider theme={theme}>
-        <ScreenProvider>
-            <CssBaseline />
-            <TopBar />
-            <Container maxWidth='xl' sx={{pt: 1}}>
-            <Grid
-              container
-              direction='column'
-              alignItems='center'
-              spacing={1}
-            >
-              <Grid item>
-                <Typography variant='h5'>
-                  Seems like something went globally wrong...
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Image
-                  src={ErrorPNG}
-                  alt="Confused"
-                  priority
-                  style={{
-                    width: '90vw',
-                    maxWidth: 309,
-                    height: 'auto',
-                  }}
-                />
-              </Grid>
-            </Grid>
-            </Container>
-        </ScreenProvider>
-      </ThemeProvider>
-    </body>
-  </html>
-
-
+export default function GlobalError({
+  error,
+}: {
+  error: Error & { digest?: string },
+  reset: () => void,
+}) {
+  return (
+    <html>
+      <body>
+        <h2>Something went wrong!</h2>
+      </body>
+    </html>
+  )
 }
