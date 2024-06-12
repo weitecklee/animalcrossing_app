63e626e64dd0ebcf37baeaaf77661e8dc94fc596
Wei-Teck Lee <arkteck@gmail.com>
2024-06-11 20:34:25 -0400

    Collapse (#14)
    * add collapse transition to villagerDialog
    
    * add customImage for use in villagerDialog
    
    * add key to CustomImage to make it reset on change

---
+++ lib/customImage.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import { rgbDataURL } from '@/lib/functions';
+import { CustomImageProps } from '../types';
+
+export default function CustomImage({ blurColor, ...props}: CustomImageProps) {
+  const { alt } = props;
+
+  return <Image
+    {...props}
+    alt={alt}
+    sizes="100vw"
+    style={{
+      objectFit: 'contain',
+    }}
+    placeholder='blur'
+    blurDataURL={rgbDataURL(blurColor)}
+  />
+}
