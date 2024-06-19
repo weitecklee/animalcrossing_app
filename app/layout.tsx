de242880693af521ca86fa897aaaaa25c1e00766
Wei-Teck Lee <arkteck@gmail.com>
2024-06-19 09:53:22 -0400

    update AppRouterCacheProvider to match nextjs version (#23)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '@/app/theme';
 import TopBar from './topBar';
