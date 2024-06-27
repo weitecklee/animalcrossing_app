f568adcd29d665085ade6c06cf87401e855ecc7c
arkteck <arkteck@gmail.com>
2024-06-26 23:22:53 -0400

    fix bug in searchLocal name matching

--- lib/searchLocal.ts
+++ lib/searchLocal.ts
@@ -3,15 +3,24 @@
 import { SearchOptions } from '@/types';
 import nookipediaData from './nookipediaData';
 
+function escapeRegExp(regexString: string) {
+  return regexString.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function convertToRegex(str: string) {
+  return new RegExp(escapeRegExp(str), 'i');
+}
+
 const villagers = Array.from(nookipediaData.values());
 
 export default async function searchLocal(
   searchOptions: SearchOptions,
 ): Promise<string[]> {
   const { name, species, personality, gender } = searchOptions;
+  const regex = convertToRegex(name);
   return villagers
     .filter((villager) => {
-      if (name.length && !villager.name.includes(name)) {
+      if (name.length && !regex.test(villager.name)) {
         return false;
       }
       if (species.length && !species.includes(villager.species)) {
