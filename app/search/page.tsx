291d1ecaa1110c30eea0edeecef8909a486d32ed
arkteck <arkteck@gmail.com>
2024-06-26 23:52:43 -0400

    use searchLocal, rearrange grid

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -4,17 +4,15 @@ import IconGrid from '@/components/iconGrid';
 import IconGridAll from '@/components/iconGridAll';
 import { NAMES, PERSONALITIES, SPECIES } from '@/lib/constants';
 import searchLocal from '@/lib/searchLocal';
-import searchMongo from '@/lib/searchMongo';
 import { SearchOptions } from '@/types';
 import {
   Autocomplete,
+  Box,
   FormControl,
-  FormControlLabel,
   Grid,
   InputLabel,
   MenuItem,
   Select,
-  Switch,
   TextField,
   Typography,
 } from '@mui/material';
@@ -57,7 +55,6 @@ export default function Page() {
   });
   const [conductSearch, setConductSearch] = useState(false);
   const [resultsFound, setResultsFound] = useState(true);
-  const [shouldUseMongo, setShouldUseMongo] = useState(false);
 
   useEffect(() => {
     setSearchOptions((prev) => ({ ...prev, name: debouncedNameFilter }));
@@ -66,115 +63,98 @@ export default function Page() {
   useEffect(() => {
     if (checkSearchOptions(searchOptions)) {
       setConductSearch(true);
-      console.time('search');
-      if (shouldUseMongo) {
-        searchMongo(searchOptions).then((res) => {
-          if (res.length) {
-            setResultsFound(true);
-            setFilteredVillagers(res);
-          } else {
-            setResultsFound(false);
-          }
-          console.timeEnd('search');
-        });
-      } else {
-        searchLocal(searchOptions).then((res) => {
-          if (res.length) {
-            setResultsFound(true);
-            setFilteredVillagers(res);
-          } else {
-            setResultsFound(false);
-          }
-          console.timeEnd('search');
-        });
-      }
+      searchLocal(searchOptions).then((res) => {
+        if (res.length) {
+          setResultsFound(true);
+          setFilteredVillagers(res);
+        } else {
+          setResultsFound(false);
+        }
+      });
     } else {
       setConductSearch(false);
       setFilteredVillagers([]);
     }
-  }, [searchOptions, shouldUseMongo]);
+  }, [searchOptions]);
 
   return (
     <>
-      <Grid container spacing={1} paddingY={1}>
-        <Grid item>
-          <Autocomplete
-            freeSolo
-            options={NAMES}
-            sx={{ width: '15rem' }}
-            inputValue={nameFilter}
-            onInputChange={(e, name) => setNameFilter(name)}
-            renderInput={(params) => <TextField {...params} label="Name" />}
-          />
-        </Grid>
-        <Grid item>
-          <Autocomplete
-            multiple
-            disableCloseOnSelect
-            options={SPECIES}
-            sx={{ width: '15rem' }}
-            value={searchOptions.species}
-            onChange={(e, species) =>
-              setSearchOptions((prev) => ({ ...prev, species }))
-            }
-            renderInput={(params) => <TextField {...params} label="Species" />}
-          />
-        </Grid>
-        <Grid item>
-          <Autocomplete
-            multiple
-            disableCloseOnSelect
-            options={PERSONALITIES}
-            sx={{ width: '15rem' }}
-            value={searchOptions.personality}
-            onChange={(e, personality) =>
-              setSearchOptions((prev) => ({ ...prev, personality }))
-            }
-            renderInput={(params) => (
-              <TextField {...params} label="Personality" />
-            )}
-          />
-        </Grid>
-        <Grid item>
-          <FormControl sx={{ width: '10rem' }}>
-            <InputLabel>Gender</InputLabel>
-            <Select
-              value={searchOptions.gender}
-              onChange={(e) =>
-                setSearchOptions((prev) => ({
-                  ...prev,
-                  gender: e.target.value,
-                }))
-              }
-              label="Gender"
-            >
-              <MenuItem value="All">All</MenuItem>
-              <MenuItem value="Female">Female</MenuItem>
-              <MenuItem value="Male">Male</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={shouldUseMongo}
-                onChange={(e) => setShouldUseMongo(e.target.checked)}
+      <Grid container spacing={2} paddingY={1}>
+        <Grid item xs={12} sm="auto">
+          <Grid container spacing={2} direction={{ xs: 'row', sm: 'column' }}>
+            <Grid item>
+              <Autocomplete
+                freeSolo
+                options={NAMES}
+                sx={{ width: '13rem' }}
+                inputValue={nameFilter}
+                onInputChange={(e, name) => setNameFilter(name)}
+                renderInput={(params) => <TextField {...params} label="Name" />}
               />
-            }
-            label="Mongo"
-          />
+            </Grid>
+            <Grid item>
+              <Autocomplete
+                multiple
+                disableCloseOnSelect
+                options={SPECIES}
+                sx={{ width: '13rem' }}
+                value={searchOptions.species}
+                onChange={(e, species) =>
+                  setSearchOptions((prev) => ({ ...prev, species }))
+                }
+                renderInput={(params) => (
+                  <TextField {...params} label="Species" />
+                )}
+              />
+            </Grid>
+            <Grid item>
+              <Autocomplete
+                multiple
+                disableCloseOnSelect
+                options={PERSONALITIES}
+                sx={{ width: '13rem' }}
+                value={searchOptions.personality}
+                onChange={(e, personality) =>
+                  setSearchOptions((prev) => ({ ...prev, personality }))
+                }
+                renderInput={(params) => (
+                  <TextField {...params} label="Personality" />
+                )}
+              />
+            </Grid>
+            <Grid item>
+              <FormControl sx={{ width: '10rem' }}>
+                <InputLabel>Gender</InputLabel>
+                <Select
+                  value={searchOptions.gender}
+                  onChange={(e) =>
+                    setSearchOptions((prev) => ({
+                      ...prev,
+                      gender: e.target.value,
+                    }))
+                  }
+                  label="Gender"
+                >
+                  <MenuItem value="All">All</MenuItem>
+                  <MenuItem value="Female">Female</MenuItem>
+                  <MenuItem value="Male">Male</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs>
+          {conductSearch ? (
+            resultsFound ? (
+              <IconGrid villagers={filteredVillagers} />
+            ) : (
+              <Typography>No results.</Typography>
+            )
+          ) : (
+            <IconGridAll />
+          )}
         </Grid>
       </Grid>
-      {conductSearch ? (
-        resultsFound ? (
-          <IconGrid villagers={filteredVillagers} />
-        ) : (
-          <Typography>No results.</Typography>
-        )
-      ) : (
-        <IconGridAll />
-      )}
     </>
   );
 }
