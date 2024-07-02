53f128a1cf950077c0ef85c0a556935e5aa39281
arkteck <arkteck@gmail.com>
2024-07-02 10:35:25 -0400

    enhance Length of Stay breakdown with more stats, use table

---
+++ app/stats/[stat]/lengthOfStay.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import VillagerIcon from '@/components/villagerIcon';
+import TitleChip from '../titleChip';
+import { Duration, History } from '@/types';
+
+export default function LengthOfStay({
+  durationData,
+  historyMap,
+}: {
+  durationData: Duration[];
+  historyMap: Map<string, History>;
+}) {
+  return (
+    <>
+      <TitleChip title={'Length of Stay Breakdown'} />
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Villager</TableCell>
+            <TableCell align="center">Length of Stay (days)</TableCell>
+            <TableCell align="center">Move-in Date</TableCell>
+            <TableCell align="center">Move-out Date</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {durationData.map((duration) =>
+            duration.villagers.map((villager) => {
+              const history = historyMap.get(villager)!;
+              return (
+                <TableRow key={villager}>
+                  <TableCell align="center">
+                    <VillagerIcon villager={villager} />
+                  </TableCell>
+                  <TableCell align="center">{duration.duration}</TableCell>
+                  <TableCell align="center">
+                    {history.startDateString}
+                  </TableCell>
+                  <TableCell align="center">
+                    {history.currentResident ? '-' : history.endDateString}
+                  </TableCell>
+                </TableRow>
+              );
+            }),
+          )}
+        </TableBody>
+      </Table>
+    </>
+  );
+}
