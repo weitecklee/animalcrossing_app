84a769aa5d7011e3b2867b311c760f92cd469503
arkteck <arkteck@gmail.com>
2025-10-15 18:22:08 -0400

    add current villagers to edit page

--- app/admin/editVillager.tsx
+++ app/admin/editVillager.tsx
@@ -9,17 +9,21 @@ import {
   Stack,
   TextField,
 } from '@mui/material';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import logout from './logout';
 import { useRouter } from 'next/navigation';
 import EditInfo from './editInfo';
 import { fixName } from '@/lib/functions';
 import VillagerInfo from '../villagers/[villager]/villagerInfo';
+import { DataContext } from '@/lib/dataContext';
 
 export default function EditVillager() {
   const [inputValue, setInputValue] = useState<string>('');
   const [villager, setVillager] = useState<string | null>('');
   const router = useRouter();
+
+  const { calculatedStats } = useContext(DataContext);
+  const { currentResidents } = calculatedStats;
 
   return (
     <Stack spacing={2}>
@@ -36,16 +40,30 @@ export default function EditVillager() {
           Logout
         </Button>
       </Box>
-      <Autocomplete
-        freeSolo
-        options={NAMES}
-        sx={{ width: '13rem' }}
-        inputValue={inputValue}
-        onInputChange={(e, name) => setInputValue(name)}
-        value={villager}
-        onChange={(e, name) => setVillager(name)}
-        renderInput={(params) => <TextField {...params} label="Villager" />}
-      />
+      <Stack direction="row" spacing={2}>
+        <Autocomplete
+          options={currentResidents}
+          sx={{ width: '13rem' }}
+          inputValue={inputValue}
+          onInputChange={(e, name) => setInputValue(name)}
+          value={villager}
+          onChange={(e, name) => setVillager(name)}
+          renderInput={(params) => (
+            <TextField {...params} label="Current Residents" />
+          )}
+        />
+        <Autocomplete
+          options={NAMES}
+          sx={{ width: '13rem' }}
+          inputValue={inputValue}
+          onInputChange={(e, name) => setInputValue(name)}
+          value={villager}
+          onChange={(e, name) => setVillager(name)}
+          renderInput={(params) => (
+            <TextField {...params} label="All Villagers" />
+          )}
+        />
+      </Stack>
       {villager && (
         <Grid container justifyContent="center" spacing={4}>
           <VillagerInfo params={{ villager: fixName(villager) }} />
